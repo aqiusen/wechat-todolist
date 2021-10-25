b9aeadebbdf16f2411a298a3fdb6e988c44f4495
sqiu <sen.qiu@thoughtworks.com>
2021-10-25 13:58:14 +0800

    优化ui

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log("res===>",res)//res===> {errMsg: "login:ok", code: "011oEZ000gbHEM1p3e20016osf4oEZ0V"}
       }
     })
   },
